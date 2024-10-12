83c7bf3522b83d07685d6be14b6649b97ee72d28
timmy-badejo <tyomibadejo@my.bcit.ca>
2024-10-12 00:25:38 -0700

    Updated structure, added components, fixed deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/http://localhost:5173/timicares_assignmennt2_web_scripting-2.git//', // This should match your GitHub repository name.
+  base: '/timicares_assignmennt2_web_scripting-2/', // GitHub repository name
   plugins: [react()],
 });
-
