16761e50d0a8e3509e0ecccf959da106a46deb04
timmy-badejo <tyomibadejo@my.bcit.ca>
2024-10-12 23:37:19 -0700

    updated gh-pages to the latest version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/timicares_assignmennt2_web_scripting-2/',  // Add the repository name here
 });
